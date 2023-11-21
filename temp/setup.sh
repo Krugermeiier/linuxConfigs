f9416d52b7dcae950ca6ee41ae2879cda347cc67
Jeevatjai Singh <74781321+Krugermeiier@users.noreply.github.com>
2023-11-21 15:24:16 -0500

    Add files via upload

---
+++ temp/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt install i3 xorg
+echo 'deb http://deb.debian.org/debian bookworm main contrib non-free' >> /etc/apt/sources.list
+dpkg --add-architecture i386
+apt update
+apt install steam alacritty firefox thunar
+startx
