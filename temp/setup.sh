43e27310e33a23e9676ba01b685e64bfd1df26c4
Jeevatjai Singh <74781321+Krugermeiier@users.noreply.github.com>
2023-11-21 15:43:49 -0500

    Update setup.sh

--- temp/setup.sh
+++ temp/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt install i3 xorg
+apt install i3 xorg lightdm
 echo 'deb http://deb.debian.org/debian bookworm main contrib non-free' >> /etc/apt/sources.list
 dpkg --add-architecture i386
 apt update
