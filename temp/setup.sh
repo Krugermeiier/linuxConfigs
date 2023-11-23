1fee94b253f4ae9bb34992bb6c9eeb2b4da3fca1
Jeevatjai Singh <74781321+Krugermeiier@users.noreply.github.com>
2023-11-22 23:37:23 -0500

    Update setup.sh

--- temp/setup.sh
+++ temp/setup.sh
@@ -3,4 +3,4 @@ apt install i3 xorg lightdm alacritty thunar
 echo 'deb http://deb.debian.org/debian bookworm main contrib non-free' >> /etc/apt/sources.list
 dpkg --add-architecture i386
 apt update
-
+systemctl enable lightdm
