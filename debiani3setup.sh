ba1411bdfcf063165bf6b7ab5f7b742fc373ca09
Jeevatjai Singh <74781321+Krugermeiier@users.noreply.github.com>
2023-11-22 23:48:24 -0500

    Add files via upload

---
+++ debiani3setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt install i3 xorg lightdm alacritty thunar rofi
+echo 'deb http://deb.debian.org/debian bookworm main contrib non-free' >> /etc/apt/sources.list
+dpkg --add-architecture i386
+apt update
+systemctl enable lightdm
