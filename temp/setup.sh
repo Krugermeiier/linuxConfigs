3a74a6f8c861160b9cf13057fd51220dfb4ceb64
Jeevatjai Singh <74781321+Krugermeiier@users.noreply.github.com>
2023-11-22 23:22:03 -0500

    Update setup.sh

--- temp/setup.sh
+++ temp/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-apt install i3 xorg lightdm
+apt install i3 xorg lightdm alacritty thunar
 echo 'deb http://deb.debian.org/debian bookworm main contrib non-free' >> /etc/apt/sources.list
 dpkg --add-architecture i386
 apt update
-apt install steam alacritty firefox thunar
-startx
+
